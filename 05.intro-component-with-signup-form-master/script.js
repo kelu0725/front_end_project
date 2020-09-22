2b4030114fb88e93f3591c34c7a192d4af493e6a
kelu0725 <klu2@alumni.cmu.edu>
2020-09-21 20:58:09 -0700

    todo: mobile screen

--- 05.intro-component-with-signup-form-master/script.js
+++ 05.intro-component-with-signup-form-master/script.js
@@ -3,18 +3,18 @@ const inputNodes = document.querySelectorAll('.input');
 
 form.addEventListener("submit", e => {
     e.preventDefault();
-    inputNodes.forEach( (inputNode) =>{
+    inputNodes.forEach((inputNode) =>{
         check(inputNode);
     })
 });
 
 function check(inputNode){
-    const id = inputNode.id;
     const val = inputNode.value;
     const parent = inputNode.parentNode;
+    const ph = inputNode.placeholder;
     if(val === ""){
         parent.classList.add('error');
-        parent.querySelector('p').innerHTML='Please enter a valid '+id;
+        parent.querySelector('p').innerHTML='Please enter a valid '+ph;
     }else{
         parent.classList.remove('error');
     }
