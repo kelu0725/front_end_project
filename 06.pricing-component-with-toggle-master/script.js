f017d339e960710cb23fd9a4fac7cf7f0af215c4
kelu0725 <klu2@alumni.cmu.edu>
2020-09-22 17:15:03 -0700

    use toggle

--- 06.pricing-component-with-toggle-master/script.js
+++ 06.pricing-component-with-toggle-master/script.js
@@ -1,12 +1,18 @@
 
 
-function changePrice(){
-    var toggle = document.getElementById('toggle');
-    var cards = document.getElementById('cards');
-    if(toggle.checked){
-        cards.classList.add("show-month");
-    }else{
-        cards.classList.remove("show-month");
-    }
+// function changePrice(){
+//     var toggle = document.getElementById('toggle');
+//     var cards = document.getElementById('cards');
+//     if(toggle.checked){
+//         cards.classList.add("show-month");
+//     }else{
+//         cards.classList.remove("show-month");
+//     }
+// }
 
+// Can also addListner to toggle, event is "change"
+function changePrice(){
+    // var toggle = document.getElementById('toggle');
+    var cards = document.getElementById('cards');
+    cards.classList.toggle("show-month");
 }
